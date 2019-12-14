2e9d4b10ab304bea9d289b80dccb741228fbadd1
Jeongho Noh <nossi8128@gmail.com>
2019-12-15 01:08:15 +0900

    file system REST API

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'root root root', app: 'Natours' });
-});
-app.post('/', (req, res) => {
-  res.send('good post');
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.get('/api/v1/tours', (req, res) => {
+  res.json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    }
+  });
 });
 app.listen(port, () => {
   console.log(`App running on port ${port}!`);
