6d4f14d967e79db7ea6db2de2603496f703fe0f5
Jeongho Noh <nossi8128@gmail.com>
2019-12-15 13:04:33 +0900

    added my own middlewareusing app.use()

--- app.js
+++ app.js
@@ -3,9 +3,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const myLogger = (req, res, next) => {
+  console.log('MyLogger');
+  next();
+};
+const myLogger2 = (req, res, next) => {
+  console.log('MyLogger2');
+  next();
+};
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+
+app.use(myLogger);
+app.use(myLogger2);
+
 app.get('/api/v1/tours', (req, res) => {
   res.json({
     status: 'success',
