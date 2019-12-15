33fd36697fb0b415c91c480fd2d2a8d0706a8ee3
Jeongho Noh <nossi8128@gmail.com>
2019-12-15 15:29:52 +0900

    implemented app.post, and used express.json middleware for body-parser of json

--- app.js
+++ app.js
@@ -3,22 +3,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const myLogger = (req, res, next) => {
-  console.log('MyLogger');
-  next();
-};
-const myLogger2 = (req, res, next) => {
-  console.log('MyLogger2');
-  next();
-};
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.use(myLogger);
-app.use(myLogger2);
-
+app.use(express.json());
 app.get('/api/v1/tours', (req, res) => {
+  console.log(req.body);
   res.json({
     status: 'success',
     results: tours.length,
@@ -26,6 +17,23 @@ app.get('/api/v1/tours', (req, res) => {
       tours
     }
   });
+});
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tours: newTour
+        }
+      });
+    }
+  );
 });
 app.listen(port, () => {
   console.log(`App running on port ${port}!`);
